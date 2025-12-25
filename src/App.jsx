4507785489bf0f6de7cdf134518567fe0b4a68f3
mrbuba <wwpptew101@gmail.com>
2025-12-25 16:00:02 +0700

    feat: Initial project setup.

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function Section() {
+  return (
+    <h1 class="text-3xl font-bold text-brown-400">
+      My First React App on Vite
+    </h1>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Section />
+    </>
+  );
+}
+
+export default App;
